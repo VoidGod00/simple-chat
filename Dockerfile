6baf70c51d1bde61047d72f2a2da9df0f4dc0493
Voidgod00 <sniyazi102938@gmail.com>
2026-02-12 19:23:09 +0530

    final

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM maven:3.9.6-eclipse-temurin-21 AS build
 COPY . .
-RUN mvn clean package -DskipTests
+RUN mvn clean package
 
 FROM eclipse-temurin:21-jre-jammy
 COPY --from=build /target/*.jar app.jar
--- src/main/java/com/test/simple_chat/service/ChatService.java
+++ src/main/java/com/test/simple_chat/service/ChatService.java
@@ -18,9 +18,9 @@ public class ChatService {
     private static final String KEY_ROOM_META = "chat:rooms";       // Hash
     private static final String KEY_ROOM_USERS = "room:%s:users";   // Set
     private static final String KEY_ROOM_HIST = "room:%s:history";  // List
-    private static final String CHANNEL_PREFIX = "chat:channel:";   // Pub/Sub
+    private static final String CHANNEL_PREFIX = "chat:channel:";   // Publish-Subscribe
 
-    // 1. Create Room (Unique Names)
+    // Create Room (Unique Names)
     public void createRoom(String roomName) {
         if (Boolean.TRUE.equals(redisTemplate.opsForHash().hasKey(KEY_ROOM_META, roomName))) {
             throw new IllegalArgumentException("Duplicate chat room name: " + roomName);
@@ -28,37 +28,38 @@ public class ChatService {
         redisTemplate.opsForHash().put(KEY_ROOM_META, roomName, Instant.now().toString());
     }
 
-    // 1. Join Room (Set)
+    // Join Room (Set)
     public void joinRoom(String roomName, String participant) {
         validateRoom(roomName);
         redisTemplate.opsForSet().add(String.format(KEY_ROOM_USERS, roomName), participant);
     }
 
-    // 2. Messaging (List + Pub/Sub)
+    //Messaging (List + Publish-Subscribe)
     public void sendMessage(String roomName, ChatMessage msg) {
         validateRoom(roomName);
 
-        // Add Timestamp
+
         ChatMessage fullMsg = new ChatMessage(
                 msg.participant(),
                 msg.message(),
+                // Add Timestamp
                 Instant.now().toString()
         );
 
-        // A. Store in List (History)
+        // Store in List (History)
         redisTemplate.opsForList().rightPush(String.format(KEY_ROOM_HIST, roomName), fullMsg);
 
-        // B. Real-time Pub/Sub
+        // Real-time Publish-Subscribe
         redisTemplate.convertAndSend(CHANNEL_PREFIX + roomName, fullMsg);
     }
 
-    // 2. Retrieve History (Last N)
+    // Retrieve History
     public List<Object> getHistory(String roomName, int limit) {
         validateRoom(roomName);
         return redisTemplate.opsForList().range(String.format(KEY_ROOM_HIST, roomName), -limit, -1);
     }
 
-    // Helper: Validation
+    // Room validation
     private void validateRoom(String roomName) {
         if (!Boolean.TRUE.equals(redisTemplate.opsForHash().hasKey(KEY_ROOM_META, roomName))) {
             throw new IllegalArgumentException("Room does not exist: " + roomName);
